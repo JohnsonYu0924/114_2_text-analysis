27cc90c6313944c81e0f43aca154761cf5280538
JohnsonYu0924 <r5213467254@gmail.com>
2025-12-02 15:59:40 +0800

    combiner

---
+++ 期末報告/script/大字串處理.py
@@ -0,0 +1,22 @@
+import os, json
+# with open("/Users/johnsonyu/Desktop/碩二上/文字分析/總統政見發表會 資料檔/114_2_text-analysis/期末報告/cleaned_data/191218_Han_cleaned.json", "r", encoding="utf-8") as f:
+#     f = json.load(f)
+#     myText = " ".join(f)
+# print(myText)
+
+path = "/Users/johnsonyu/Desktop/碩二上/文字分析/總統政見發表會 資料檔/114_2_text-analysis/期末報告/cleaned_data/"
+os.chdir(path)
+dirList = [i for i in os.listdir(path) if i.endswith(".json")]
+print(dirList)
+
+def combiner(data_name):
+    with open(data_name, "r", encoding="utf-8") as f:
+        f = json.load(f)
+        myText = " ".join(f)
+    with open(data_name, "w", encoding="utf-8") as f:
+        json.dump(myText, f, ensure_ascii=False, indent=4)
+    
+
+for i in dirList:
+    print(i)
+    combiner(i)
