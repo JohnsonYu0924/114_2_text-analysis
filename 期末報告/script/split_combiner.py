00f631f227dd666241c96142b28f43020136a9db
JohnsonYu0924 <r5213467254@gmail.com>
2025-12-05 15:14:47 +0800

    .txt

--- 期末報告/script/split_combiner.py
+++ 期末報告/script/split_combiner.py
@@ -1,22 +1,24 @@
-import os, json
+import os, json, re
 # with open("/Users/johnsonyu/Desktop/碩二上/文字分析/總統政見發表會 資料檔/114_2_text-analysis/期末報告/cleaned_data/191218_Han_cleaned.json", "r", encoding="utf-8") as f:
 #     f = json.load(f)
 #     myText = " ".join(f)
 # print(myText)
 
-path = "/Users/johnsonyu/Desktop/碩二上/文字分析/總統政見發表會 資料檔/114_2_text-analysis/期末報告/cleaned_data/"
+path = "/Users/johnsonyu/Desktop/碩二上/文字分析/總統政見發表會 資料檔/114_2_text-analysis/期末報告/raw_data/"
 os.chdir(path)
 dirList = [i for i in os.listdir(path) if i.endswith(".json")]
 print(dirList)
 
-def combiner(data_name):
+def combiner(data_name, savefolder_path):
     with open(data_name, "r", encoding="utf-8") as f:
         f = json.load(f)
         myText = " ".join(f)
-    with open(data_name, "w", encoding="utf-8") as f:
-        json.dump(myText, f, ensure_ascii=False, indent=4)
-
-
+        data_name = re.sub(r".json", "_combine.txt", data_name)
+    with open(savefolder_path + data_name, "w", encoding="utf-8") as f:
+        f.write(myText)
+        
+        
+path = "/Users/johnsonyu/Desktop/碩二上/文字分析/總統政見發表會 資料檔/114_2_text-analysis/期末報告/raw_data/combine/"
 for i in dirList:
     print(i)
-    combiner(i)
+    combiner(i, path)
